aa7fab40adc13bd9714beca5f0506985f4b9561a
David Kam <davidkam@sierra-bravo.com>
2012-05-15 17:42:12 -0500

    adding config file

---
+++ php/docs/config.php
@@ -0,0 +1,7 @@
+<?php
+define('BASEPATH', dirname(__FILE__) . '/..');
+define('SCRIPTPATH', BASEPATH . '/scripts/');
+define('BINPATH', BASEPATH . '/bin/');
+define('INCLUDEPATH', BASEPATH . '/includes/');
+
+define('TRUE_POLARITY', false);
